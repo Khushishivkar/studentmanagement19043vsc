4837a0574de4acf6ccc7c99d717fdb44964038c4
Khushishivkar <shivkarkhushi4@gmail.com>
2026-03-05 21:39:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
+from datetime import date
+
+app = Flask(__name__)
+
+def get_db():
+    conn = sqlite3.connect("attendance.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route("/")
+def index():
+    conn = get_db()
+    students = conn.execute("SELECT * FROM students").fetchall()
+    attendance = conn.execute("SELECT * FROM attendance").fetchall()
+    conn.close()
+    return render_template("index.html", students=students, attendance=attendance)
+
+@app.route("/add", methods=["POST"])
+def add_student():
+    name = request.form["name"]
+
+    conn = get_db()
+    conn.execute("INSERT INTO students (name) VALUES (?)", (name,))
+    conn.commit()
+    conn.close()
+
+    return redirect("/")
+
+@app.route("/mark/<int:id>/<status>")
+def mark_attendance(id, status):
+    today = date.today()
+
+    conn = get_db()
+    conn.execute(
+        "INSERT INTO attendance (student_id, status, date) VALUES (?, ?, ?)",
+        (id, status, today),
+    )
+    conn.commit()
+    conn.close()
+
+    return redirect("/")
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    conn = get_db()
+    conn.execute("DELETE FROM students WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    conn = sqlite3.connect("attendance.db")
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT)"
+    )
+
+    conn.execute(
+        """CREATE TABLE IF NOT EXISTS attendance (
+        id INTEGER PRIMARY KEY,
+        student_id INTEGER,
+        status TEXT,
+        date TEXT
+        )"""
+    )
+
+    conn.close()
+
+    app.run(debug=True)
